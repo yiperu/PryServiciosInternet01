15f8e616004496311d4fe683bd57f358ac4c0d98
Henry AT <yiperu@gmail.com>
2014-03-30 21:02:34 -0300

    Configuramos el proyecto de tal modo que tenga las cosas iniciales que toda aplicacion con conexion a internet requiere como minimo

--- src/com/apps4s/pryserviciosinternet01/MainActivity.java
+++ src/com/apps4s/pryserviciosinternet01/MainActivity.java
@@ -1,7 +1,11 @@
 package com.apps4s.pryserviciosinternet01;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,8 +14,35 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy);
 	}
 
+	public boolean isNetworkAvailable(){
+		ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+		if (networkInfo != null && networkInfo.isConnected()) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isWifiConnection(){
+		ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		boolean isWifi = networkInfo.isConnected();
+		return isWifi;
+	}
+	
+	public boolean isMobileConnection(){
+		ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+		boolean isMobile = networkInfo.isConnected();
+		return isMobile;
+	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
